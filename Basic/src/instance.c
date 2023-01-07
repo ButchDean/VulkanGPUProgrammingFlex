091276491843f9cb709ef51df010cd76b33543cd
Dean Butcher <butchdeanca@gmail.com>
2023-01-07 01:45:05 -0500

    Now link to Vulkan and added basic GetPhysicalDevice() definition.

--- Basic/src/instance.c
+++ Basic/src/instance.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <vulkan/vulkan.h>
 #include <instance.h>
 
+static VkInstance Instance = VK_NULL_HANDLE;
+static VkPhysicalDevice PhysicalDevice = VK_NULL_HANDLE;
+
 void CreateInstance() {}
-void GetPhysicalDevice() {}
+
+void GetPhysicalDevice() {
+    VkPhysicalDevice devices[100];
+    uint32_t count = 100;
+
+    if(vkEnumeratePhysicalDevices(Instance, &count, devices) != VK_SUCCESS) {
+        puts("Eniumerating physicaldevices failed.\n");
+        return;
+    }
+
+    PhysicalDevice = devices[0];
+
+    VkPhysicalDeviceProperties deviceProperties;
+    VkPhysicalDeviceFeatures deviceFeatures;
+
+    vkGetPhysicalDeviceProperties(PhysicalDevice, &deviceProperties);
+    vkGetPhysicalDeviceFeatures(PhysicalDevice, &deviceFeatures);
+}
