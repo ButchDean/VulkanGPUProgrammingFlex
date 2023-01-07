c74b020977c9dd76455be075dca767eaba085fbb
Dean Butcher <butchdeanca@gmail.com>
2023-01-07 02:06:07 -0500

    Added CreateInstance() to basic project.

--- Basic/src/instance.c
+++ Basic/src/instance.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include <vulkan/vulkan.h>
 #include <instance.h>
 
 static VkInstance Instance = VK_NULL_HANDLE;
 static VkPhysicalDevice PhysicalDevice = VK_NULL_HANDLE;
 
-void CreateInstance() {}
+void CreateInstance() {
+    const char* layers[] = { "VK_LAYER_KHRONOS_validation" };
+
+    VkInstanceCreateInfo instanceCreateInfo;
+    memset(&instanceCreateInfo, 0, sizeof(instanceCreateInfo));
+
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.ppEnabledExtensionNames = layers;
+    instanceCreateInfo.enabledLayerCount = 1;
+
+    if(vkCreateInstance(&instanceCreateInfo, NULL, &Instance) != VK_SUCCESS) {
+        perror("Create Instance failed.\n");
+    }
+}
 
 void GetPhysicalDevice() {
     VkPhysicalDevice devices[100];
     uint32_t count = 100;
 
     if(vkEnumeratePhysicalDevices(Instance, &count, devices) != VK_SUCCESS) {
-        puts("Eniumerating physicaldevices failed.\n");
+        perror("Enumerating physicaldevices failed.\n");
         return;
     }
 
